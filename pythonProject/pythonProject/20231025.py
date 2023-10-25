d78d5401e818faac19ff23d65ee6ce51a305e962
zhouyongcai <807923011@qq.com>
2023-10-25 20:47:08 +0800

    修改字符串111

--- pythonProject/pythonProject/20231025.py
+++ pythonProject/pythonProject/20231025.py
@@ -1,3 +1,18 @@
 str_a = "abcdefg"
 print(str_a[6])
 print(str_a[1:5])
+print("hogwarts teacher is %s"%"张家辉")
+# demo = "hogwarts is a {}"
+# demo_res = demo.format("school")
+# print(demo_res)
+
+demo = "hogwarts is a {0} {1}"  #demo是原始的变量内容
+demo_res = demo.format("very good","school")  #demo_res是替换过后的变量内容
+print(demo_res)
+
+name = "Bob"
+school = "hogwarts"
+print(f"我的名字叫做{name}，毕业于{school}")   #通过 f"(变量名)
+
+a = ["h","o","g","w","a","r","t","s"]
+print("".join(a))
